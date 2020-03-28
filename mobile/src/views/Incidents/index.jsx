9adadda785d272e7f44135528d7781c2eb819332
BuildTools <lorenzooguimaraes@gmail.com>
2020-03-28 07:08:55 -0300

    feat(mobile): Add IncidentList directly and load next page of incidents when reach the list end

--- mobile/src/views/Incidents/index.jsx
+++ mobile/src/views/Incidents/index.jsx
@@ -1,28 +1,56 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 
-import { Container, HeaderText, Strong, Title, Description } from "./styles";
+import {
+  Container,
+  HeaderText,
+  Strong,
+  Title,
+  Description,
+  IncidentList,
+} from "./styles";
 
-import IncidentList from "../../components/IncidentList";
 import Header from "../../components/Header";
 import Api from "../../services/api";
+import Incident from "../../components/Incident";
 
 const Incidents = () => {
   const [incidents, setIncidents] = useState([]);
   const [total, setTotal] = useState(0);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    async function loadIncidents() {
-      try {
-        const response = await Api.get("incidents");
-        setIncidents(response.data);
-        setTotal(response.headers["x-total-count"]);
-      } catch {
-        console.error("Could'nt get incidents");
-      }
+  const loadIncidents = useCallback(async () => {
+    if (loading) {
+      return;
     }
 
-    loadIncidents().then();
+    if (total > 0 && incidents.length >= total) {
+      return;
+    }
+
+    try {
+      const response = await Api.get("incidents", {
+        params: {
+          page,
+        },
+      });
+
+      setLoading(true);
+
+      setIncidents(($) => [...response.data, ...$]);
+      setTotal(response.headers["x-total-count"]);
+      setPage(($) => $++);
+
+      setLoading(false);
+    } catch {
+      console.error("Could'nt get incidents");
+    }
   }, []);
+
+  useEffect(() => {
+    loadIncidents().then();
+  }, [loadIncidents]);
+
   return (
     <Container>
       <Header>
@@ -34,7 +62,14 @@ const Incidents = () => {
       <Title>Seja bem-vindo!</Title>
       <Description>Escolha um dos casos abaixo e salve o dia!</Description>
 
-      <IncidentList incidents={incidents} />
+      <IncidentList
+        data={incidents}
+        keyExtractor={() => Math.random().toString()}
+        showsVerticalScrollIndicator={false}
+        onEndReached={loadIncidents}
+        onEndReachedThreshold={0.3}
+        renderItem={({ item }) => <Incident incident={item} />}
+      />
     </Container>
   );
 };
